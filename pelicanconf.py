b4cd992484b3db6e5e4a1279d5277a2077063a4d
Kyle Cranmer <kyle.cranmer@nyu.edu>
2015-05-10 11:49:57 +0200

    typo in plugin_paths

--- pelicanconf.py
+++ pelicanconf.py
@@ -48,7 +48,7 @@ CODE_DIR = 'downloads/code'
 NOTEBOOK_DIR = 'downloads/notebooks'
 
 
-PLUGIN_PATHs = ['pelican-plugins']
+PLUGIN_PATHS = ['pelican-plugins']
 PLUGINS = ['summary', 'liquid_tags.img', 'liquid_tags.video',
 			'liquid_tags.youtube', 'render_math',
            'liquid_tags.include_code', 'liquid_tags.notebook',
