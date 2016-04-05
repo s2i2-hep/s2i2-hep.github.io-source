b1a2fda172a7a60dc0bae159f030a552c8fbe78d
Peter Elmer <Peter.Elmer@cern.ch>
2016-04-05 13:19:22 +0200

    More changes from diana-hep to s2i2-hep

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://diana-hep.org'
+SITEURL = 'http://s2i2-hep.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
