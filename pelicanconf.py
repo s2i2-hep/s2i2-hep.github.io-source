4cdbe343097185c76c09fb68f90b1e8daa04f04e
Peter Elmer <Peter.Elmer@cern.ch>
2016-06-16 13:29:07 +0200

    try simplex theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -53,10 +53,10 @@ THEME = 'pelican-themes/pelican-bootstrap3'
 PYGMENTS_STYLE='default'
 #PYGMENTS_STYLE='friendly'
 
-#BOOTSTRAP_THEME='simplex'
+BOOTSTRAP_THEME='simplex'
 #BOOTSTRAP_THEME='yeti'
 #BOOTSTRAP_THEME='superhero' #nice but, background doesn't work well with code as is
-BOOTSTRAP_THEME='cosmo'
+#BOOTSTRAP_THEME='cosmo'
 #BOOTSTRAP_THEME='paper'
 DISPLAY_BREADCRUMBS=False
 
